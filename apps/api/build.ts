389a27bbcd8a8822773f538129a6a44194c5d84d
Aleksa S. Stevic <aleksa.stevic26@gmail.com>
2024-02-26 18:06:51 +0100

    added docker

--- apps/api/build.ts
+++ apps/api/build.ts
@@ -1,9 +1,14 @@
 import * as esbuild from 'esbuild'
 
-await esbuild.build({
-    entryPoints: ['src/index.ts'],
-    bundle: true,
-    platform: 'node',
-    target: 'node20',
-    outdir: 'dist',
-})
+async function main() {
+    await esbuild.build({
+        entryPoints: ['src/index.ts'],
+        bundle: true,
+        treeShaking: false,
+        platform: 'node',
+        target: 'node20',
+        outdir: 'dist',
+    })
+}
+
+main()
